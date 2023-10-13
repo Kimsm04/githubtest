901479e432e06c5076d70de442db36746176156b
Arafchi <123809613+Arafchi@users.noreply.github.com>
2023-10-13 13:37:42 +0900

    수치 같은 거 조절하거나 코드 예쁘게 만들면 될 듯? 이외 구현 안한 거 구현하거

--- mugunghwa_chung.c
+++ mugunghwa_chung.c
@@ -4,7 +4,6 @@
 #include <Windows.h>
 #include <conio.h>
 #include <stdbool.h>
-#define TICK 100		// 0.1초
 // *: 벽
 // #: 영희
 // @: 뒤돌아본 영희
@@ -17,8 +16,6 @@ typedef enum _direction
 	IDLE = 1, LEFT, UP, DOWN, RIGHT
 }direction;
 typedef enum _state { alive, dead, finished }state;
-typedef struct {char state[20];}STATE;
-typedef struct {int live; }LIVE;
 typedef struct _tickState
 {
 	double goalTick;
@@ -28,14 +25,15 @@ void gotoxy(int row, int col);
 void draw(void);
 bool moveOn(point*, direction);	// 움직일 수 없으면 false, 있으면 움직이고 true 반환
 double getTick();
-void SayFlower();
-void p_alive(void);
+int SayFlower();	// 무궁화꽃이피었습니다 에서 얼만큼 출력했는지 int형으로 반환
+void killPlayer();
+void playerDialog(void);
 char map[9][40], front[9][40];
 bool running = true;
-
+state states[5] = { alive, };
+state fStates[5] = { -1,-1,-1,-1,-1 };
 int main() {
 	srand((unsigned int)time(NULL));
-	
 	//맵 생성
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 40; j++) {
@@ -53,28 +51,21 @@ int main() {
 
 	// 참가자 위치 초기화
 	point pl[5] = { {38,2}, {38,3},{38,4},{38,5},{38,6} };
-	bool states[5] = { alive, };
 
 	tickState plTicks[5] = { {100,0}, {1000,0}, {400,0}, {3000,0}, {1600,0}, };
-	tickState tagger = {100,0};	// 영희
-	//random으로 하면 안되나
+	tickState taggerSaying = {100,0};	// 영희
+	tickState taggerWatching = {3000,0}; // d영희
+	bool isWatching = false;
+
 	map[pl[0].y][pl[0].x] = '0';
 	map[pl[1].y][pl[1].x] = '1';
 	map[pl[2].y][pl[2].x] = '2';
 	map[pl[3].y][pl[3].x] = '3';
 	map[pl[4].y][pl[4].x] = '4';
 
-	p_alive();
-	bool fstates[5] = { alive, };
 	while (running) {
 		draw();
-		for(int i=0;i<5;i++)
-			if (states[i] == dead) {
-				if (fstates[i] != dead) {
-					p_alive(i);
-					fstates[i] = dead;
-				}
-			}
+		playerDialog();
 		//플레이어 조작
 		if (states[0] == alive && _kbhit() && plTicks[0].goalTick <= plTicks[0].cntTick)
 		{
@@ -105,9 +96,9 @@ int main() {
 			if ((pl[0].x == 1) || (pl[0].x == 2 && 3 <= pl[0].y && pl[0].y <= 5))
 				states[0] = finished;
 		}
-		//ai
 		for (int i = 1; i < 5; i++)
 		{
+			if( isWatching != true || rand()%10 == 0)	// 보고 있지않거나, 보고있더라고 10%의 확률로 실행
 			if (states[i] == alive && plTicks[i].goalTick <= plTicks[i].cntTick)
 			{
 				plTicks[i].cntTick = 0;
@@ -128,22 +119,65 @@ int main() {
 
 		}
 		
-		if (tagger.goalTick <= tagger.cntTick)
+		// 무궁화꽃이 피었습니다 출력
+		if (isWatching)
 		{
-			tagger.cntTick = 0;
-			SayFlower();
+			if (taggerWatching.goalTick <= taggerWatching.cntTick)
+			{
+				isWatching = false;
+				taggerWatching.cntTick = 0;
+				map[3][1] = '#';
+				map[4][1] = '#';
+				map[5][1] = '#';
+				gotoxy(10, 0);
+				printf("                                                               ");
+			}
+			else
+			{
+				map[3][1] = '@';
+				map[4][1] = '@';
+				map[5][1] = '@';
+				killPlayer();
+			}
 		}
-
+		else
+		{
+			if (taggerSaying.goalTick <= taggerSaying.cntTick)
+			{
+				int cnt = SayFlower();
+				if (cnt < 10)
+					taggerSaying.cntTick = taggerSaying.goalTick * (1 - (cnt + rand() % 11) * 0.1);	//0.5~0.1
+				else
+					taggerSaying.cntTick = taggerSaying.goalTick * ((cnt - 15 + rand() % 11) * 0.1);	//0.5 ~ 1
+				//tagger.cntTick = 0;
+				taggerSaying.goalTick += 100;
+				if (20 <= cnt)
+					isWatching = true;
+			}
+			
+		}
+		
 		//tick 계산
 		double tick = getTick();
 		for (int i = 0; i < 5; i++) plTicks[i].cntTick += tick;
-		tagger.cntTick += tick;
+		taggerSaying.cntTick += tick;
+		if(isWatching) taggerWatching.cntTick += tick;
 
+		for (int i = 0; i <= 5; i++)
+			if (i == 5)
+				running = false;
+			else if(states[i] != alive)
+				continue;
+			else
+				break;
 
 	}
-
+	draw();
 	return 0;
 }
+
+
+
 
 bool moveOn(point* pt, direction dir)
 {
@@ -191,19 +225,7 @@ double getTick()
 	return clock() - temp;
 
 }
-void p_alive(i) {
-	STATE state_arr[2] = { {"alive" }, { "dead " } };
-	static LIVE live_arr[5] = { {0},{0},{0},{0} ,{0} };
-	live_arr[i].live = 1;
-	gotoxy(14,0);
-	printf("no. of players left: 5\n");
-	printf("player 0: %s\n", state_arr[live_arr[0].live]);
-	printf("player 1: %s\n", state_arr[live_arr[1].live]);
-	printf("player 2: %s\n", state_arr[live_arr[2].live]);
-	printf("player 3: %s\n", state_arr[live_arr[3].live]);
-	printf("player 4: %s\n", state_arr[live_arr[4].live]);
-}
-void SayFlower()
+int SayFlower()
 {
 	static int cnt = 0;
 	const char* sentence = "무궁화꽃이피었습니다";
@@ -212,9 +234,52 @@ void SayFlower()
 		gotoxy(10, cnt);
 		printf("%c%c", sentence[cnt],sentence[cnt+1]);
 		cnt += 2;
-		return;
+		return cnt;
 	}
 	cnt = 0;
+	return cnt;
+}
+void playerDialog() {
+	static int fplayerCnt=0;
+	int playerCnt = 5;
+	for (int i = 0; i < 5; i++)
+		if (states[i] == dead)
+			playerCnt--;
+	
+	if (fplayerCnt != playerCnt)
+	{
+		gotoxy(14, 0);
+		fplayerCnt = playerCnt;
+		printf("no. of players left: %d\n", playerCnt);
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		if (fStates[i] != states[i])
+		{
+			fStates[i] = states[i];
+			gotoxy(15 + i, 0);
+			printf("player %d: ", i);
+			if (states[i] == dead)
+				printf("dead!\n");
+			else
+				printf("alive\n");
+		}
+	}
+}
+void killPlayer()
+{
+	for (int i = 0; i < 9; i++)
+		for (int j = 0; j < 40; j++)
+			if (front[i][j] != map[i][j] && map[i][j] != '#' && map[i][j] != '@')
+			{
+				states[map[i][j] - '0'] = dead;
+				for (int k = j - 1; 0 < k; k--)
+				{
+					if ('0' <= map[i][k] && map[i][k] <= '9')
+						states[map[i][j] - '0'] = alive;
+				}
+
+			}
 }
 void gotoxy(int row, int col) {
 	COORD pos = { col, row };
@@ -223,7 +288,6 @@ void gotoxy(int row, int col) {
 		pos
 	);
 }
-//맵 함수
 void draw(void) {
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 40; j++) {
